d7b8eea10b159e620aa2fd6170693c7b202720f1
Sy Le <sycung9001@gmail.com>
2024-08-05 14:49:10 +0700

    setup redux

---
+++ store/cartSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: "Hi",
+  reducers: {},
+});
+
+export default cartSlice.reducer;
